4279ef81588dc036bae7e0ae5d1363c6fd3a27a3
Luke Harrison <wlharr04@gmail.com>
2023-08-10 19:04:42 -0400

    created Data Frame and saved as CSV file

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,6 @@
 # add your code here
+import pandas as pd
+
+fruit_sales = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]},index=['2017 Sales', '2018 Sales'])
+
+fruit_sales.to_csv('fruit.csv')
