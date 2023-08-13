5173175825d80a585d09d2ff1630c66a57eb145c
Luke Harrison <wlharr04@gmail.com>
2023-08-12 21:28:20 -0400

    updated to adhere to established conventions

--- fruitsales.py
+++ fruitsales.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-fruit_sales = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]},index=['2017 Sales', '2018 Sales'])
+df_fruit_sales = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]},index=['2017 Sales', '2018 Sales'])
 
-fruit_sales.to_csv('fruit.csv')
+df_fruit_sales.to_csv('fruit.csv')
